e0a3cfb96446ceb9f7abe8c3bc41b9c666a7efcd
androkingdom <androkingdom1@gmail.com>
2024-11-10 20:24:33 +0530

    initial commit

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    return render(request, "apps/index.html")
+
+def about(request):
+    return render(request, "apps/about.html")
+
+def contact(request):
+    return render(request, "apps/contact.html")
